38d7ceba9a8d0dc6fc8b9f037f7c5c07ad3b8ab8
Moudook <moudook@gmail.com>
2026-08-16 21:14:17 +0530

    restructured the codebase

--- temporary/2.py
+++ temporary/2.py
@@ -25,6 +25,11 @@ label = df[['label']]
 # Random weight initialization (one per feature)
 from random import randrange as rdr
 weights = [rdr(-2, 2), rdr(-2, 2), rdr(-2, 2)]
+W1 = [[rdr(-2, 2), rdr(-2, 2), rdr(-2, 2)],
+    [rdr(-2, 2), rdr(-2, 2), rdr(-2, 2)]]
+# WEIGHTS FOR hidden layers
+W2 = [rdr(-2, 2), rdr(-2, 2), rdr(-2, 2)]
+# weights for output layer
 
 epoch = 200
 B = 0.7  # sigmoid slope
@@ -35,15 +40,20 @@ for i in range(1, epoch + 1):
     misclass = 0
     eta = 1 / i  # decaying learning rate
     while count < len(data):
-        y = np.dot(weights, data.iloc[count])  # weighted sum
-        AF = ActivationFunction(y, B)           # sigmoid activation
-        error = label.iloc[count].label - y    # prediction error
+        y1 = np.dot(W1.iloc[1], data.iloc[count])
+        h1 = ActivationFunction(y1,B)
+        y2 = np.dor(W1.iloc[2], data.iloc[count])
+        h2 = ActivationFunction(y2,B)
+        # weighted sum for i hidden layer that contain two neuronsans passed vis activation funciton ie sigmoid
+
+        #refrencing  this website https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+        y3 = np.dot(W2,[h1,h2,1])
+        AF = ActivationFunction(y3, B)           # sigmoid activation
+        error = label.iloc[count].label - AF    # prediction error
         if error != 0:
             # Gradient update toward correct label
             sigmoid_derivative = B * AF * (1 - AF)
-            weights = weights + np.array(
-                eta * error * sigmoid_derivative * data.iloc[count]
-            )
+            weights = weights + np.array(eta * error * sigmoid_derivative * data.iloc[count])
             misclass += 1
         count += 1
 
@@ -62,3 +72,17 @@ plt.plot(df['x1'], line, c='red')
 plt.xlabel("x1")
 plt.ylabel("x2")
 plt.show()
+
+
+
+
+
+DL_Assignments/
+  Assignment_1/
+    GroupXX_Assignment1_code/
+      himanshu_classif_ds1/    <- loader + perceptron + plots + metrics + notes
+      anshuman_classif_ds2/    <- same, independent
+      sidharth_regression/     <- same, independent
+      data/                    <- group datasets (all three read-only from here)
+    GroupXX_Assignment1_report.pdf
+    GroupXX_Assignment1_code.zip
