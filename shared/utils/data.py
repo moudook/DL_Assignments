b1875488c24166080d5d07ed3e28bd2ecf1a8fb1
Anshuman Rahul <anshumanrahul06@gmail.com>
2026-08-19 22:32:33 +0530

    feat: Implemented train_test_slit in utils.py

--- shared/utils/data.py
+++ shared/utils/data.py
@@ -2,3 +2,17 @@
 # to have shared code for loading the data as it'll just depend
 # on the assingment on the type of data being dealt with
 # But could have some funcitions regarding data loading like dealing with csvs or something
+
+import numpy as np
+
+def train_test_split(X: np.ndarray, y: np.ndarray, train_ratio: float=0.7, seed: int=42):
+    if(train_ratio<=0 or train_ratio>=1):
+        raise ValueError("Train ratio must be between 0 and 1")
+    num_rows = X.shape[0]
+    indices = np.arange(num_rows)
+    rng = np.random.default_rng(seed)
+    rng.shuffle(indices)
+    train_count = int(num_rows*train_ratio)
+    train_indices = indices[:train_count]
+    test_indices = indices[train_count:]
+    return X[train_indices], X[test_indices], y[train_indices], y[test_indices]
